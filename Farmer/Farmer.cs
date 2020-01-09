8f2d614265e9b9a2f63b9d2c210cba3ececd096c
Bekzat2021 <bekzat.work@gmail.com>
2020-01-09 20:30:18 +0500

    Chapter 5 Page 269

---
+++ Farmer/Farmer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmer
+{
+    class Farmer
+    {
+        public const int FeedMultiplier = 30;
+
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                public int BagsOfFeed { get; set; }
+    }
+        }
+    }
+}
