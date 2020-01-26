810fb9d07359df51869d95286cbec6e0df60da4f
Bekzat2021 <bekzat.work@gmail.com>
2020-01-26 20:05:26 +0500

    Chapter 6 Page 286

--- Form1.cs
+++ Form1.cs
@@ -18,22 +18,32 @@ namespace DinnerParty
         {
             InitializeComponent();
 
-            
-        }
-        
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
+            dinnerParty = new DinnerParty((int)numericUpDown1.Value, HealtyhBox.Checked, FancyBox.Checked);
             
         }
 
         private void FancyBox_CheckedChanged(object sender, EventArgs e)
         {
-            
+            dinnerParty.FancyDecorations = FancyBox.Checked;
+            DisplayDinnerPartyCost();
         }
-
+        
         private void HealtyhBox_CheckedChanged(object sender, EventArgs e)
         {
-            
+            dinnerParty.HealthyOption = HealtyhBox.Checked;
+            DisplayDinnerPartyCost();
+        }
+        
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerParty.NumberOfPeople = (int)numericUpDown1.Value;
+            DisplayDinnerPartyCost();
+        }
+
+        private void DisplayDinnerPartyCost()
+        {
+            decimal Cost = dinnerParty.Cost;
+            label3.Text = Cost.ToString("c");
         }
     }
 }
