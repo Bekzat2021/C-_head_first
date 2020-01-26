96d8918b14141caa14bdb3741e13479f91a858ca
Bekzat2021 <bekzat.work@gmail.com>
2020-01-26 17:58:20 +0500

    Page 177 not ended

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinnerParty
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerParty;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+        
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void HealtyhBox_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
