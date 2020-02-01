06ace1feca81a52ccce55c05f2aea7891cdbb761
Bekzat2021 <bekzat.work@gmail.com>
2020-02-01 22:34:15 +0500

    Chapter 6 Page 311

---
+++ JewelTheif.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class JewelTheif:Locksmith
+    {
+        private Jewels stolenJewels = null;
+        public void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine("I'm stealing the contents! " + stolenJewels.Sparkle());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Owner owner = new Owner();
+            Safe safe = new Safe();
+            JewelTheif jewelTheif = new JewelTheif();
+            jewelTheif.OpenSafe(safe, owner);
+            Console.ReadKey();
+        }
+    }
+}
