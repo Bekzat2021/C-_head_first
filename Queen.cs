49903a9f5cd005635c018b5a36d5252e3aeafc56
Bekzat2021 <bekzat.work@gmail.com>
2020-02-05 23:22:50 +0500

    Chapter 6 Page 328

---
+++ Queen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeHiveManagment
+{
+    class Queen
+    {
+        private Worker[] workers;
+        private int shiftNumber;
+
+        public bool AssignWork()
+        {
+
+        }
+    }
+}
